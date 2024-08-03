dd13afb1693dc7a2fecd012ac3cd1aa42a1fae9c
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-08-03 13:38:54 -0400

    another commit

--- solution3.py
+++ solution3.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 3 below
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return(fibonacci(n-1)+fibonacci(n-2))
+
+n=10
+for i in range(n):
+    print(fibonacci(i))
